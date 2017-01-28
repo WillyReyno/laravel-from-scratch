afbe750931cd3ad0fd8a0268d8dfed995a4f78e0
WillyReyno <reyno.willy@gmail.com>
2017-01-28 18:52:43 +0100

    Display content in views

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-    show
+    <div class="col-sm-8 blog-main">
+
+        <div class="blog-post">
+
+            <h2 class="blog-post-title"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+
+            <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
+
+            <p>{{ $post->content }}</p>
+
+        </div><!-- /.blog-post -->
+
+    </div><!-- /.blog-main -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,9 @@ use App\Models\Task;
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
+
+
 Route::get('/', 'PostsController@index');
-
 Route::get('/posts/create', 'PostsController@create');
-
 Route::get('/posts/{post}', 'PostsController@show');
-
 Route::post('/posts', 'PostsController@store');
