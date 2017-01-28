7593d5860f896be838e0c18f9d2706a917849e42
WillyReyno <reyno.willy@gmail.com>
2017-01-28 15:51:29 +0100

    Model, Eloquent...

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,20 @@
 |
 */
 
+use App\Models\Task;
+
 Route::get('/tasks', function () {
 
     $tasks = DB::table('tasks')->latest()->get();
+
+    $tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{id}', function($id) {
 
-   $task = DB::table('tasks')->find($id);
+   $task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });
