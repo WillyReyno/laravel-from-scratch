8affbf7385397e224687323de33ee0e76b8e5068
WillyReyno <reyno.willy@gmail.com>
2017-01-28 16:18:52 +0100

    Created TasksController

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,5 @@
 
 use App\Models\Task;
 
-Route::get('/tasks', function () {
-
-    $tasks = DB::table('tasks')->latest()->get();
-
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{id}', function($id) {
-
-   $task = Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
